50135686aa89176af3f361ff85245a95c87caf30
taylor cassidy <taylorcassidy64@gmail.com>
2017-12-22 12:09:24 -0500

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='spacyss',
-    version='0.1',
+    version='0.0.1',
     packages=['seg', 'seg.newline'],
     url='https://github.com/tc64/spacyss',
     license='',
