51839bb06c574c6900b087ec46ca07b9a5fb03e0
taylor cassidy <taylorcassidy64@gmail.com>
2017-12-22 12:45:17 -0500

    changed how newline token is detected using spacy is_space bool

--- seg/newline/segmenter.py
+++ seg/newline/segmenter.py
@@ -2,12 +2,19 @@ import re
 
 
 class NewLineSegmenter(object):
-    ALL_SPACE = re.compile('\s+$')
     def __init__(self):
         pass
 
+    def is_nl_token(self, t):
+
+        # if a token consists of all space, and has at least one newline char, we segment as a sentence.
+        if t.is_space and '\n' in t.text:
+            return True
+        else:
+            return False
+
     def set_sent_starts(self, doc):
-        if self.ALL_SPACE.match(doc[0].text):
+        if self.is_nl_token(doc[0]):
             doc[0].is_sent_start = True
         else:
             doc[0].is_sent_start = False
@@ -15,7 +22,7 @@ class NewLineSegmenter(object):
             return doc
 
         for t in doc[1:]:
-            if self.ALL_SPACE.match(doc[t.i-1].text) and not self.ALL_SPACE.match(t.text):
+            if self.is_nl_token(doc[t.i-1]) and not self.is_nl_token(t):
                 t.is_sent_start = True
             else:
                 t.is_sent_start = False
@@ -41,5 +48,5 @@ if __name__ == "__main__":
 
     import spacy
     nlp = spacy.load('en')
-    nlp.add_pipe(NewlineSegmenter().set_sent_starts, name='sentence_segmenter', before='parser')
+    nlp.add_pipe(NewLineSegmenter().set_sent_starts, name='sentence_segmenter', before='parser')
     doc = nlp(samples)
