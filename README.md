3f8a49f7b86e8477d631358a9256d22efb14849f
taylor cassidy <taylorcassidy64@gmail.com>
2017-12-22 12:38:19 -0500

    updated version

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ sentences in text are separated by one or more newline characters.
 
 
 ```python
-from spacyss.seg.newline import set_sent_starts as newline_segmenter
+from seg.newline.segmenter import NewLineSegmenter
 import spacy
 
+nlseg = NewLineSegmenter()
+
 nlp = spacy.load('en')
-nlp.add_pipe(newline_segmenter, name='sentence_segmenter', before='parser')
+nlp.add_pipe(nlseg.set_sent_starts, name='sentence_segmenter', before='parser')
 
 doc = nlp(my_doc_text)
 ```
--- seg/newline/segmenter.py
+++ seg/newline/segmenter.py
@@ -1,29 +1,36 @@
+import re
 
-def set_sent_starts(doc):
-    if set(doc[0].text) != {'\n'}:
-        doc[0].is_sent_start = True
-    else:
-        doc[0].is_sent_start = False
-    if len(doc) == 1:
-        return doc
 
-    for t in doc[1:]:
-        if set(doc[t.i-1].text) == {'\n'} and set(t.text) != {'\n'}:  # we are at a sentence start
-            t.is_sent_start = True
+class NewLineSegmenter(object):
+    ALL_SPACE = re.compile('\s+$')
+    def __init__(self):
+        pass
+
+    def set_sent_starts(self, doc):
+        if self.ALL_SPACE.match(doc[0].text):
+            doc[0].is_sent_start = True
         else:
-            t.is_sent_start = False
+            doc[0].is_sent_start = False
+        if len(doc) == 1:
+            return doc
 
-    return doc
+        for t in doc[1:]:
+            if self.ALL_SPACE.match(doc[t.i-1].text) and not self.ALL_SPACE.match(t.text):
+                t.is_sent_start = True
+            else:
+                t.is_sent_start = False
+
+        return doc
 
 
 if __name__ == "__main__":
     samples = """
 
-    this is my first sentence
-    this is my second one
+         this is my first sentence
+    this is my second one    
     third one!
 
-    definitely
+             definitely
 
 
 
@@ -34,5 +41,5 @@ if __name__ == "__main__":
 
     import spacy
     nlp = spacy.load('en')
-    nlp.add_pipe(set_sent_starts, name='sentence_segmenter', before='parser')
+    nlp.add_pipe(NewlineSegmenter().set_sent_starts, name='sentence_segmenter', before='parser')
     doc = nlp(samples)
